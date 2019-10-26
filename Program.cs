90f05f2ee2012e360393afe95bfb41ed922bf0bf
pranjalofficial <pranjalofficial1996@gmail.com>
2019-10-26 12:27:29 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string name;
+           System.Console.WriteLine("What is your Name:");
+           name = Console.ReadLine();
+           System.Console.WriteLine("Hello" +" "+ name);
+        }
+    }
+}
