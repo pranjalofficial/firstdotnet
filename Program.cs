18f2ce67d0e1a8fc2be0bd622f15c23c4d27d167
pranjalofficial <pranjalofficial1996@gmail.com>
2019-10-26 23:02:11 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -6,10 +6,51 @@ namespace DotNet
     {
         static void Main(string[] args)
         {
-           string name;
-           System.Console.WriteLine("What is your Name:");
-           name = Console.ReadLine();
-           System.Console.WriteLine("Hello" +" "+ name);
+            int selection;
+            int firstno;
+            int secondno;
+            int result;
+           System.Console.WriteLine("Welcome to my Calculator Application:");
+           System.Console.WriteLine("Select what you want to do:");
+           System.Console.WriteLine("1. Addition");
+           System.Console.WriteLine("2. Subtraction");
+           System.Console.WriteLine("3. Multiplication");
+           System.Console.WriteLine("4. Divison");
+           selection = System.Convert.ToInt32(Console.Readline());
+
+            if(selection == 1)
+            {
+               firstno = System.Convert.ToInt32(Console.Readline());
+               secondno = System.Convert.ToInt32(Console.Readline());
+                result = firstno + secondno;
+                System.Console.WriteLine("Answer is:"+result);
+            }
+            else if(selection == 2)
+            {
+                firstno = System.Convert.ToInt32(Console.Readline());
+               secondno = System.Convert.ToInt32(Console.Readline());
+                result = firstno - secondno;
+                System.Console.WriteLine("Answer is:"+result);
+            } 
+            else if(selection == 3)
+            {
+                firstno = System.Convert.ToInt32(Console.Readline());
+               secondno = System.Convert.ToInt32(Console.Readline());
+                result = firstno * secondno;
+                System.Console.WriteLine("Answer is:"+result);
+            } 
+            else if(selection == 4)
+            {
+                firstno = System.Convert.ToInt32(Console.Readline());
+               secondno = System.Convert.ToInt32(Console.Readline());
+                result = firstno/secondno;
+                System.Console.WriteLine("Answer is:"+result);
+            } 
+            else
+            {
+                System.Console.WriteLine("Wrong Selection");
+            }   
+            }
         }
     }
 }
